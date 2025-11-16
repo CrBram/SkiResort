4eda0f9424484b366243dabc48a6f0b81b0916bf
Bramcr <crBram98@gmail.com>
2025-11-16 14:59:07 +0100

    added point lights in cabin

--- src/Experience.tsx
+++ src/Experience.tsx
@@ -14,6 +14,24 @@ const Experience = () => {
       <Environment preset="sunset" />
       <directionalLight position={[5, 5, 5]} intensity={0.5} castShadow />
       <directionalLight position={[-3, 2, 1]} intensity={0.5} color="#ffa500" />
+      {/* Cabin 1 light */}
+      <pointLight
+        position={[-1.45, 1.35, -1.2]}
+        intensity={1.5}
+        scale={0.2}
+        color="#ffd700"
+        distance={5}
+        decay={2}
+      />
+      {/* Cabin 2 light */}
+      <pointLight
+        position={[-0.4, 3.0, 2.3]}
+        intensity={1.5}
+        scale={0.2}
+        color="#ffd700"
+        distance={5}
+        decay={2}
+      />
       <Resort />
     </>
   );
