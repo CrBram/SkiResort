c95765a265f52afce913f16f70006004b01e23ce
Bramcr <crBram98@gmail.com>
2025-11-19 18:56:59 +0100

    added les arcs page

--- src/Experience_LesArcs.tsx
+++ src/Experience_LesArcs.tsx
@@ -16,18 +16,36 @@ const Experience_LesArcs = () => {
       <Environment preset="sunset" />
       <directionalLight position={[5, 5, 5]} intensity={0.5} castShadow />
       <directionalLight position={[-3, 2, 1]} intensity={0.5} color="#ffa500" />
-      {/* Cabin 1 light */}
+      {/* Cabin 1 light - cabin at [-1.256, 1.717, 126.83], offset similar to original */}
       <pointLight
-        position={[-1.45, 1.35, -1.2]}
+        position={[-1.559, 2.016, 126.75]}
         intensity={1.5}
         scale={0.2}
         color="#ffd700"
         distance={5}
         decay={2}
       />
-      {/* Cabin 2 light */}
+      {/* Cabin 2 light - cabin at [-1.231, 2.684, 129.587], offset similar to original */}
       <pointLight
-        position={[-0.4, 3.0, 2.3]}
+        position={[-1.485, 2.745, 129.501]}
+        intensity={1.5}
+        scale={0.2}
+        color="#ffd700"
+        distance={5}
+        decay={2}
+      />
+      {/* Cabin 3 light - cabin at [-1.862, 1.546, 133.913], offset similar to original */}
+      <pointLight
+        position={[-2.165, 1.845, 133.833]}
+        intensity={1.5}
+        scale={0.2}
+        color="#ffd700"
+        distance={5}
+        decay={2}
+      />
+      {/* Cabin 4 light - cabin at [2.502, 4.125, 126.728], offset similar to original */}
+      <pointLight
+        position={[2.248, 4.186, 126.642]}
         intensity={1.5}
         scale={0.2}
         color="#ffd700"
